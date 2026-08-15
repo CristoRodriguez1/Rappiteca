30f9c1b9aec7623c3ae0ff212cd21d39f43c50fd
Emanuell <etorresl@eafit.edu.co>
2026-08-15 17:23:11 -0500

    Hasheo de contraseñas agregado

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=128) 
     role = models.CharField(max_length=5, default='stu')
 
     def __str__(self):
